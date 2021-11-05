4941ebccc18258a7cf4af96807594c1bc8698f45
masafumi <masa455.wv@gmail.com>
2021-11-06 06:36:25 +0900

    firstcommit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+// 「dbname」「port」「host」「username」「password」を設定
+// DB接続情報　　　　↓ここだけ自分のデータベース名に変更！
+define('DSN', 'mysql:host=localhost;dbname=deploy_test');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+try {
+  $pdo = new PDO(DSN, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+  echo json_encode(["db error" => "{$e->getMessage()}"]);
+  exit(); // 「dbError:...」が表示されたらdb接続でエラーが発生していることがわかる
+}
+function h($str){
+  return htmlspecialchars($str, ENT_QUOTES, 'utf-8');
+}
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once('./functions.php');
+$stmt = $pdo->prepare('SELECT * FROM users_table');
+$stmt->execute();
+$all_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// echo json_encode($all_rows);
+
+$stack = array();
+foreach ($all_rows as $key => $value) {
+  $array = (object) array(
+      "name" => "$value[u_name]",
+      "address" => "$value[email]"
+  );
+  array_push($stack, $array);
+}
+echo "<pre>";
+echo json_encode($stack, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+echo "</pre>";
