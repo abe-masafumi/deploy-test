ff76f61f9c20abe35c38e0ab4dd4d29735be89b0
masafumi <masa455.wv@gmail.com>
2021-11-08 19:12:38 +0900

    fix

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+
+// gasとの連携でAPIとしてobjectを返す！
+// リンク:https://docs.google.com/spreadsheets/d/1rC68STkg-JS72EmSD_ZINM2LvoXLnIGR2STNhelvXA4/edit#gid=0
+
+
 require_once('./functions.php');
 $stmt = $pdo->prepare('SELECT * FROM users_table');
 $stmt->execute();
 $all_rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 // echo json_encode($all_rows);
+
+// all_rowsだけならこれだけでもいける
+// echo json_encode($violater_list);
 
 $stack = array();
 foreach ($all_rows as $key => $value) {
